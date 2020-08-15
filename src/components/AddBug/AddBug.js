4941093570e1b4861571c31e7a0c7d58ac73bbff
Elias Habash <eliashabash23@yahoo.com>
2020-08-15 13:55:06 -0700

    Completed AddBug Form component

--- src/components/AddBug/AddBug.js
+++ src/components/AddBug/AddBug.js
@@ -2,64 +2,111 @@ import React, { Component } from 'react'
 import { Card, Form, Button, Col } from 'react-bootstrap';
 
 class AddBug extends Component {
+
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			description: "",
+			created: "",
+			due: "",
+			reporter: "",
+			status: "",
+			severity: ""
+		}
+	}
+	
+	submitBug = (e) => {
+		alert(this.state.description);
+		e.preventDefault();
+	}
+
+	bugChange = (e) => {
+		e.preventDefault();
+		this.setState({
+			[e.target.name] : e.target.value
+		})
+	}
+
 	render() {
 		return (
 			<Card className="border border-dark bg-dark text-white">
 				<Card.Header>Add Bug</Card.Header>
-					<Form>
+					<Form onSubmit={this.submitBug} id="BugFormId">
 						<Card.Body>
-							<Form.Group>
+							<Form.Group controlId="formGridDescription">
 								<Form.Label>Description</Form.Label>
-								<Form.Control 
-									type="text" 
+								<Form.Control required
+									type="text" name="description"
 									placeholder="Description"
+									value={this.state.description}
+									onChange={this.bugChange}
 									className="bg-dark text-white"/>
+									<Form.Control.Feedback type="invalid">
+										Please provide a valid description.
+									</Form.Control.Feedback>
 							</Form.Group>
 							<Form.Row>
-								<Form.Group as={Col}>
+								<Form.Group as={Col} controlId="formGridCreated">
 									<Form.Label>Created</Form.Label>
-									<Form.Control 
-										type="date" 
-										className="bg-dark text-white"/>
+									<Form.Control required
+										type="date" name="created"
+										className="bg-dark text-white"
+										value={this.state.created}
+										onChange={this.bugChange}/>
 								</Form.Group>
-									<Form.Group as={Col}>
+									<Form.Group as={Col} controlId="formGridDue">
 									<Form.Label>Due</Form.Label>
-									<Form.Control 
-										type="date" 
-										className="bg-dark text-white"/>
+									<Form.Control required
+										type="date" name="due"
+										className="bg-dark text-white"
+										value={this.state.due}
+										onChange={this.bugChange}/>
 								</Form.Group>
 							</Form.Row>
 							<Form.Row>
-								<Form.Group as={Col}>
+								<Form.Group as={Col} controlId="formGridReporter">
 									<Form.Label>Reporter</Form.Label>
-										<Form.Control
+										<Form.Control required
 											className="bg-dark text-white"
-											as="select">	
+											as="select"
+											name="reporter"
+											value={this.state.reporter}
+											onChange={this.bugChange}>	
 										</Form.Control>
 								</Form.Group>
-								<Form.Group as={Col}>
+								<Form.Group as={Col} controlId="formGridStatus">
 									<Form.Label>Status</Form.Label>
-										<Form.Control
+										<Form.Control required
 											className="bg-dark text-white"
-											as="select">	
+											as="select"
+											name="status"
+											value={this.state.status}
+											onChange={this.bugChange}>	
 											<option>In Progress</option>
 											<option>Closed</option>
 										</Form.Control>
 								</Form.Group>
-								<Form.Group as={Col}>
+								<Form.Group as={Col} controlId="formGridSeverity">
 									<Form.Label>Severity</Form.Label>
-										<Form.Control
+										<Form.Control required
 											className="bg-dark text-white"
-											as="select">
+											as="select"
+											name="severity"
+											value={this.state.severity}
+											onChange={this.bugChange}>
 											<option>Minor</option>
 											<option>Major</option>	
 										</Form.Control>
 								</Form.Group>
-								<Form.Group as={Col}>
+								<Form.Group as={Col} controlId="formGridReproducable">
 									<Form.Label>Reproducable?</Form.Label>
 										<Form.Control
 											className="bg-dark text-white"
-											as="select">
+											as="select"
+											name="reproducable"
+											value={this.state.reproducable}
+											onChange={this.bugChange}>
 											<option>Yes</option>
 											<option>No</option>	
 										</Form.Control>
@@ -68,7 +115,7 @@ class AddBug extends Component {
 						</Card.Body>
 					</Form>
 				<Card.Footer style={{"textAlign": "right"}}>
-						<Button variant="success" type="submit">
+						<Button variant="success" type="submit" onClick={this.submitBug}>
 							Submit
 						</Button>
 					</Card.Footer>
